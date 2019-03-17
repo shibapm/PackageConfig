0451f4fef7a2606a85c85c8c412c4a905d22d137
Igor Muzyka <igormuzyka42@gmail.com>
2019-03-17 07:39:21 +0200

    Updated script

--- Sources/PackageConfigExecutable/main.swift
+++ Sources/PackageConfigExecutable/main.swift
@@ -6,7 +6,6 @@ let script =
 """
 mkdir -p ./Sources/PackageConfigs/
 touch ./Sources/PackageConfigs/PackageConfigs.swift
-echo '// Do not delete this file or it's target, it is requried to build dylibs for the Packages you installed which depend on PackageConfig for their own package configuration' > ./Sources/PackageConfigs/PackageConfigs.swift
 swift build --target PackageConfigs
 """
 
