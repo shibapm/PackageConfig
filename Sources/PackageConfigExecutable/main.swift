374d99c507be9be4d0a8cc0676530b2a8d712b4c
Igor Muzyka <igormuzyka42@gmail.com>
2019-03-17 15:45:30 +0200

    Updated package-config executable script

--- Sources/PackageConfigExecutable/main.swift
+++ Sources/PackageConfigExecutable/main.swift
@@ -4,6 +4,7 @@ import Foundation
 let process = Process()
 let script =
 """
+swift package resolve
 mkdir -p ./Sources/PackageConfigs/
 touch ./Sources/PackageConfigs/PackageConfigs.swift
 swift build --target PackageConfigs
