8ac4c108f2d96ca237d0ed529ae6d075170a7cbf
Franco Meloni <franco.meloni91@gmail.com>
2019-04-07 13:45:36 +0100

    Revert loader and writer changes

--- Sources/PackageConfig/Loader.swift
+++ Sources/PackageConfig/Loader.swift
@@ -1,8 +1,11 @@
-import Foundation
+
+import func Foundation.NSTemporaryDirectory
+import class Foundation.FileManager
+import class Foundation.JSONDecoder
 
 enum Loader {
 	static func load<T: PackageConfig>() throws -> T {
-        let packageConfigJSON = NSHomeDirectory() + "/.package-config/" + T.fileName
+		let packageConfigJSON = NSTemporaryDirectory() + T.fileName
 
 		guard let data = FileManager.default.contents(atPath: packageConfigJSON) else {
 			throw Error("Could not find a file at \(packageConfigJSON) - something went wrong with compilation step probably")
--- Sources/PackageConfig/Writer.swift
+++ Sources/PackageConfig/Writer.swift
@@ -1,26 +1,24 @@
-import Foundation
+
+import class Foundation.JSONEncoder
+import class Foundation.FileManager
+import func Foundation.NSTemporaryDirectory
 
 enum Writer {
 
 	static func write<T: PackageConfig>(configuration: T) {
-        let jsonFolder = NSHomeDirectory() + "/.package-config"
-        let jsonPath = jsonFolder + "/" + T.fileName
-        
-        if !FileManager.default.fileExists(atPath: jsonFolder) {
-            try! FileManager.default.createDirectory(atPath: jsonFolder, withIntermediateDirectories: false, attributes: [:])
-        }
+		let packageConfigJSON = NSTemporaryDirectory() + T.fileName
 		let encoder = JSONEncoder()
 
 		do {
 			let data = try encoder.encode(configuration)
 
-			if !FileManager.default.createFile(atPath: jsonPath, contents: data, attributes: nil) {
-				debugLog("PackageConfig: Could not create a temporary file for the PackageConfig: \(jsonPath)")
+			if !FileManager.default.createFile(atPath: packageConfigJSON, contents: data, attributes: nil) {
+				debugLog("PackageConfig: Could not create a temporary file for the PackageConfig: \(packageConfigJSON)")
 			}
 		} catch {
 			debugLog("Package config failed to encode configuration \(configuration)")
 		}
 
-		debugLog("written to path: \(jsonPath)")
+		debugLog("written to path: \(packageConfigJSON)")
 	}
 }
